b922d8399444db5a78a3ab8636cc4861cad42fa1
RedWood <mackey91@gmail.com>
2015-08-03 12:11:02 -0700

    ui-router needs work

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,16 @@
-angular.module('app', []);
+angular.module('app', ['ui-router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partials/home.html'
+        })
+        .state('about', {
+          url: '/about',
+          templateUrl: 'partials/about.html'
+          
+        });
+    
+      $urlRouterProvider.otherwise('/home');    
+});

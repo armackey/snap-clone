9f2c779dfbb4fec054e59964d99faa5fe8793fb4
RedWood <mackey91@gmail.com>
2015-08-24 09:14:25 -0700

    test

--- client/js/services/auth.token.js
+++ client/js/services/auth.token.js
@@ -1,23 +1,25 @@
 angular
-  .module('auth.token', [])
-    .factory('AuthToken', ['$window', function ($window) {
-        var self = this;
+    .module('auth.token', [])
+    .factory('AuthToken', ['$window',
+        function($window) {
+            var self = this;
 
-        // gets token 
-        self.getToken = function () {
-          return $window.localStorage.getItem('token');
-        };
+            // gets token 
+            self.getToken = function() {
+                return $window.localStorage.getItem('token');
+            };
 
-        // adds and removes token on login/signup
-        self.setToken =  function (token) {
-          if (token) {
-            $window.localStorage.setItem('token', token);
-            console.log('hi');
-          } else {
-            $window.localStorage.removeItem('token');
-            console.log('bye');
-          }
-        };
+            // adds and removes token on login/signup
+            self.setToken = function(token) {
+                if (token) {
+                    $window.localStorage.setItem('token', token);
+                    console.log('hi');
+                } else {
+                    $window.localStorage.removeItem('token');
+                    console.log('bye');
+                }
+            };
 
-        return self;
-}]);
+            return self;
+        }
+    ]);
